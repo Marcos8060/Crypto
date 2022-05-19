f16ca91a678821a5f83067468a2b5eaab6f67cea
Marcos8060 <marcosgav80@gmail.com>
2022-05-19 10:10:12 +0300

    added a background image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,27 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
 
 const useStyles = makeStyles(() => ({
-    
+    banner:{
+        backgroundImage: 'url(./banner2.jpg)' 
+    },
+    bannerContent:{
+        height: '400',
+        display: 'flex',
+        flexDirection: 'column',
+        paddingTop: 24,
+        justifyContent: 'space-around'
+    }
 }))
 const Banner = () => {
+  const classes = useStyles();
   return (
-    <div className=''>
+    <div className={classes.banner}>
+        <Container className={classes.bannerContent}>
 
+        </Container>
     </div>
   )
 }
