33c1c6fcffa382bfd740682f9fae00ad0a26b546
Marcos8060 <marcosgav80@gmail.com>
2022-05-19 10:25:22 +0300

    added the banner content

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,26 +1,50 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import {Typography} from '@material-ui/core'
 
 
 const useStyles = makeStyles(() => ({
     banner:{
-        backgroundImage: 'url(./banner2.jpg)' 
+        backgroundImage: 'url(./banner2.jpg)'
     },
     bannerContent:{
-        height: '400',
+        height: 400,
         display: 'flex',
         flexDirection: 'column',
-        paddingTop: 24,
+        paddingTop: 25,
         justifyContent: 'space-around'
-    }
+    },
+
 }))
 const Banner = () => {
   const classes = useStyles();
+
   return (
     <div className={classes.banner}>
         <Container className={classes.bannerContent}>
-
+            <div className={classes.tagline}>
+               <Typography
+               variant='h2'
+               style={{
+                   fontWeight: 'bold',
+                   marginBottom: 14,
+                   fontFamily: 'Montserrat'
+               }}
+               >
+                   Crypto Hunter
+                </Typography>  
+                <Typography
+               variant='subtitle2'
+               style={{
+                   color: 'darkgrey',
+                   textTransform: 'capitalize',
+                   fontFamily: 'Montserrat'
+               }}
+               >
+                 Get all the Info about your favorite Crypto Currency
+                </Typography>
+            </div>
         </Container>
     </div>
   )
