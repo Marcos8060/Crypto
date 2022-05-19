766152b3149960929a7b11e3a37582490bf27c39
Marcos8060 <marcosgav80@gmail.com>
2022-05-19 11:59:02 +0300

    added a coinslist component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import CoinsList from '../components/CoinsList'
 
 const Home = () => {
   return (
     <div>
       <Banner />
+      <CoinsList />
     </div>
   )
 }
