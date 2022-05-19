127404d6bb3f2e5733b22554d66349346c89a372
Marcos8060 <marcosgav80@gmail.com>
2022-05-19 12:22:39 +0300

    created a search bar

--- src/components/CoinsList.js
+++ src/components/CoinsList.js
@@ -2,23 +2,51 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { CoinList } from '../config/api'
 import { CryptoState } from '../CryptoContext'
+import Container from '@material-ui/core/Container';
+import { createTheme, TextField, ThemeProvider, Typography } from '@material-ui/core'
 
 const CoinsList = () => {
     const { currency } = CryptoState();
     const [coins,setCoins] = useState([])
     const [loading,setLoading] = useState(false)
+    const [search,setSearch] = useState()
 
     useEffect(() =>{
         const getCoinsTable = async() =>{
+            setLoading(true)
            const { data }  = await axios.get(CoinList(currency))
            setCoins(data)
+           setLoading(false)
         }
         getCoinsTable();
-    },[])
-    console.log(coins)
+    },[currency])
+
+    const darkTheme = createTheme({
+        palette:{
+            primary:{
+                main: '#fff',
+            },
+            type: 'dark'
+        }
+    })
 
   return (
-    <div>CoinsList</div>
+      <ThemeProvider theme={darkTheme}>
+          <Container style={{ textAlign: 'center'}}>
+              <Typography
+              variant='h4'
+              style={{ margin: 18, fontFamily: 'Montserrat'}}
+              >
+                  Cryptocurrency Prices by Market Gap
+              </Typography>
+              <TextField 
+              label='Search for a Crypto Currency...'
+              variant='outlined'
+              styel={{ marginBottom: 20, width: '100%'}}
+              onChange={(e) => setSearch(e.target.value)}
+              />
+          </Container>
+      </ThemeProvider>
   )
 }
 
